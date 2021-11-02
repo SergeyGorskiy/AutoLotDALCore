a5fb661fb8688cb95780c99be01201fe29ca5673
Sergey Nazhestkin <9497020@mail.ru>
2021-11-02 17:51:53 +0300

    Добавлен WEB-проект AutoLotMVCCore для реализации веб-интерфейса.

---
+++ AutoLotMVCCore/Program.cs
@@ -0,0 +1,31 @@
+using AutoLotDALCore.DataInitialization;
+using AutoLotDALCore.EF;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AutoLotMVCCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webHost = CreateHostBuilder(args).Build();
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<AutoLotContext>();
+                MyDataInitializer.RecreateDatabase(context);
+                MyDataInitializer.InitializeData(context);
+            }
+            
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
